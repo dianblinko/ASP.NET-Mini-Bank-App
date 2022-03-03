627a6bda03e0384bdcb2570700c180217d5718cf
dianblinko <chepurov.artem@gmail.com>
2022-03-03 23:14:04 +0500

    Добавьте файлы проекта.

---
+++ Minibank.Core/CurrencyСonversion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Minibank.Core
+{
+    public class CurrencyСonversion : ICurrencyСonversion
+    {
+        private readonly IExchangeRates _ExchangeRates;
+        public CurrencyСonversion(IExchangeRates ExchangeRates)
+        {
+            _ExchangeRates = ExchangeRates; 
+        }
+        public int Converting(int sum, string code)
+        {
+            var value = _ExchangeRates.Get(code);
+            if (value * sum < 0)
+            {
+                throw new UserFriendlyException("Отрицательная сумма");
+            }
+            else
+            {
+                return value * sum;
+            }
+            
+        }
+        
+    }
+}
---
+++ Minibank.Data/ExchangeRates.cs
@@ -0,0 +1,23 @@
+﻿using Minibank.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minibank.Data
+{
+    public class ExchangeRates : IExchangeRates
+    {
+        Random random;
+        public ExchangeRates()
+        {
+            random = new Random();
+        }
+        public int Get(string code)
+        {
+            return random.Next(1,150);
+        }
+
+    }
+}
